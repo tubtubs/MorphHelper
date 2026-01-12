78e24402e1ec24a37f97e0451839d9b51feef339
Tubtubs <96892771+tubtubs@users.noreply.github.com>
2026-01-12 13:52:52 -0700

    Update MountList.lua

--- MountList.lua
+++ MountList.lua
@@ -1,470 +1,530 @@
 MH_MountList = {
 	[1] = {
+		["ModelName"] = "Horse",
+		["ID"] = 235,
+		["TextureVariation1"] = "HorseSkinEvil",
+	},
+	[2] = {
+		["ModelName"] = "Horse",
+		["ID"] = 236,
+		["TextureVariation1"] = "HorseSkinWhite",
+	},
+	[3] = {
+		["ModelName"] = "Horse",
+		["ID"] = 237,
+		["TextureVariation1"] = "HorseSkinPalamino",
+	},
+	[4] = {
+		["ModelName"] = "Horse",
+		["ID"] = 238,
+		["TextureVariation1"] = "HorseSkinPinto",
+	},
+	[5] = {
+		["ModelName"] = "Horse",
+		["ID"] = 239,
+		["TextureVariation1"] = "HorseSkinBlack",
+	},
+	[6] = {
+		["ModelName"] = "DireWolf",
+		["ID"] = 246,
+		["TextureVariation1"] = "DireWolfSkinDarkBlack",
+	},
+	[7] = {
 		["ModelName"] = "RidingDireWolf",
 		["ID"] = 247,
 		["TextureVariation1"] = "RidingDireWolfSkinBrown",
 	},
-	[2] = {
+	[8] = {
 		["ModelName"] = "RidingDireWolf",
 		["ID"] = 1166,
 		["TextureVariation1"] = "RidingDireWolfSkinLightBlue",
 	},
-	[3] = {
+	[9] = {
+		["ModelName"] = "Horse",
+		["ID"] = 2325,
+		["TextureVariation1"] = "HorseSkinChessnut",
+	},
+	[10] = {
 		["ModelName"] = "RidingDireWolf",
 		["ID"] = 2326,
 		["TextureVariation1"] = "RidingDireWolfSkinReddishBrown",
 	},
-	[4] = {
+	[11] = {
 		["ModelName"] = "RidingDireWolf",
 		["ID"] = 2327,
 		["TextureVariation1"] = "RidingDireWolfSkinDarkGrey",
 	},
-	[5] = {
+	[12] = {
 		["ModelName"] = "RidingDireWolf",
 		["ID"] = 2328,
 		["TextureVariation1"] = "RidingDireWolfSkinDarkBrown",
 	},
-	[6] = {
+	[13] = {
+		["ModelName"] = "Nightmare",
+		["ID"] = 2346,
+		["TextureVariation1"] = "HorseSkinEvil2",
+	},
+	[14] = {
 		["ModelName"] = "RidingHorse",
 		["ID"] = 2402,
 		["TextureVariation1"] = "RidingHorseSkinBlack",
 	},
-	[7] = {
+	[15] = {
 		["ModelName"] = "RidingHorse",
 		["ID"] = 2404,
 		["TextureVariation1"] = "RidingHorseSkinBrown",
 	},
-	[8] = {
+	[16] = {
 		["ModelName"] = "RidingHorse",
 		["ID"] = 2405,
 		["TextureVariation1"] = "RidingHorseSkinChessnut",
 	},
-	[9] = {
+	[17] = {
 		["ModelName"] = "RidingHorse",
 		["ID"] = 2408,
 		["TextureVariation1"] = "RidingHorseSkinPalamino",
 	},
-	[10] = {
+	[18] = {
 		["ModelName"] = "RidingHorse",
 		["ID"] = 2409,
 		["TextureVariation1"] = "RidingHorseSkinPinto",
 	},
-	[11] = {
+	[19] = {
 		["ModelName"] = "RidingHorse",
 		["ID"] = 2410,
 		["TextureVariation1"] = "RidingHorseSkinWhite",
 	},
-	[12] = {
+	[20] = {
 		["ModelName"] = "RidingRam",
 		["ID"] = 2736,
 		["TextureVariation1"] = "RidingRamSkinGrey",
 	},
-	[13] = {
+	[21] = {
 		["ModelName"] = "RidingRam",
 		["ID"] = 2784,
 		["TextureVariation1"] = "RidingRamSkinBlack",
 	},
-	[14] = {
+	[22] = {
 		["ModelName"] = "RidingRam",
 		["ID"] = 2785,
 		["TextureVariation1"] = "RidingRamSkinBrown",
 	},
-	[15] = {
+	[23] = {
 		["ModelName"] = "RidingRam",
 		["ID"] = 2786,
 		["TextureVariation1"] = "RidingRamSkinWhite",
 	},
-	[16] = {
+	[24] = {
 		["ModelName"] = "RidingRam",
 		["ID"] = 2787,
 		["TextureVariation1"] = "RidingRamSkinBlue",
 	},
-	[17] = {
+	[25] = {
 		["ModelName"] = "RidingFrostSabre",
 		["ID"] = 4805,
 		["TextureVariation1"] = "RidingTigerSkinYellow",
 	},
-	[18] = {
+	[26] = {
 		["ModelName"] = "RidingRaptor",
 		["ID"] = 4806,
 		["TextureVariation1"] = "RidingRaptorSkinEmerald",
 	},
-	[19] = {
+	[27] = {
 		["ModelName"] = "RidingFrostSabre",
 		["ID"] = 6080,
 		["TextureVariation1"] = "RidingTigerSkinWhite",
 	},
-	[20] = {
+	[28] = {
 		["ModelName"] = "RidingFrostSabre",
 		["ID"] = 6442,
 		["TextureVariation1"] = "RidingTigerSkinBrown",
 	},
-	[21] = {
+	[29] = {
 		["ModelName"] = "RidingFrostSabre",
 		["ID"] = 6444,
 		["TextureVariation1"] = "RidingTigerSkinSnow",
 	},
-	[22] = {
+	[30] = {
 		["ModelName"] = "RidingFrostSabre",
 		["ID"] = 6447,
 		["TextureVariation1"] = "RidingTigerSkinYellow",
 	},
-	[23] = {
+	[31] = {
 		["ModelName"] = "RidingFrostSabre",
 		["ID"] = 6448,
 		["TextureVariation1"] = "RidingTigerSkinDark",
 	},
-	[24] = {
+	[32] = {
 		["ModelName"] = "RidingRaptor",
 		["ID"] = 6469,
 		["TextureVariation1"] = "RidingRaptorSkinTomato",
 	},
-	[25] = {
+	[33] = {
 		["ModelName"] = "RidingRaptor",
 		["ID"] = 6471,
 		["TextureVariation1"] = "RidingRaptorSkinIvory",
 	},
-	[26] = {
+	[34] = {
 		["ModelName"] = "RidingRaptor",
 		["ID"] = 6472,
 		["TextureVariation1"] = "RidingRaptorSkinTurquoise",
 	},
-	[27] = {
+	[35] = {
 		["ModelName"] = "RidingRaptor",
 		["ID"] = 6473,
 		["TextureVariation1"] = "RidingRaptorSkinViolet",
 	},
-	[28] = {
+	[36] = {
 		["ModelName"] = "MechaStrider",
 		["ID"] = 6569,
 		["TextureVariation1"] = "GnomeMechaStriderSkinBlue",
 	},
-	[29] = {
+	[37] = {
 		["ModelName"] = "MechaStrider",
 		["ID"] = 9473,
 		["TextureVariation1"] = "GnomeMechaStriderSkinRed",
 	},
-	[30] = {
+	[38] = {
 		["ModelName"] = "MechaStrider",
 		["ID"] = 9474,
 		["TextureVariation1"] = "GnomeMechaStriderSkinBlack",
 	},
-	[31] = {
+	[39] = {
 		["ModelName"] = "MechaStrider",
 		["ID"] = 9476,
 		["TextureVariation1"] = "GnomeMechaStriderSkin",
 	},
-	[32] = {
+	[40] = {
 		["ModelName"] = "RidingFrostSabre",
 		["ID"] = 9695,
 		["TextureVariation1"] = "RidingTigerSkinNostripeWhite",
 	},
-	[33] = {
+	[41] = {
 		["ModelName"] = "RidingFrostSabre",
 		["ID"] = 9714,
 		["TextureVariation1"] = "RidingTigerSkinGold",
 	},
-	[34] = {
+	[42] = {
 		["ModelName"] = "RidingFrostSabre",
 		["ID"] = 9991,
 		["TextureVariation1"] = "RidingTigerSkinBlack",
 	},
-	[35] = {
+	[43] = {
 		["ModelName"] = "RidingFrostSabre",
 		["ID"] = 10426,
 		["TextureVariation1"] = "RidingTigerSkinLavender",
 	},
-	[36] = {
+	[44] = {
 		["ModelName"] = "MechaStrider",
 		["ID"] = 10661,
 		["TextureVariation1"] = "MechaStriderSkin_GrayGreen",
 	},
-	[37] = {
+	[45] = {
 		["ModelName"] = "MechaStrider",
 		["ID"] = 10662,
 		["TextureVariation1"] = "MechaStriderSkin_BluePurple",
 	},
-	[38] = {
+	[46] = {
 		["ModelName"] = "MechaStrider",
 		["ID"] = 10664,
 		["TextureVariation1"] = "MechaStriderSkin_RedBlue",
 	},
-	[39] = {
+	[47] = {
 		["ModelName"] = "MechaStrider",
 		["ID"] = 10666,
 		["TextureVariation1"] = "MechaStriderSkin_WhiteBlue",
 	},
-	[40] = {
+	[48] = {
 		["ModelName"] = "MechaStrider",
 		["ID"] = 10667,
 		["TextureVariation1"] = "MechaStriderSkin_YellowGreen",
 	},
-	[41] = {
+	[49] = {
 		["ModelName"] = "RidingUndeadHorse",
 		["ID"] = 10670,
 		["TextureVariation1"] = "UndeadHorseSkinRed",
 	},
-	[42] = {
+	[50] = {
 		["ModelName"] = "RidingUndeadHorse",
 		["ID"] = 10671,
 		["TextureVariation1"] = "UndeadHorseSkinBlue",
 	},
-	[43] = {
+	[51] = {
 		["ModelName"] = "RidingUndeadHorse",
 		["ID"] = 10672,
 		["TextureVariation1"] = "UndeadHorseSkinYellow",
 	},
-	[44] = {
+	[52] = {
 		["ModelName"] = "RidingUndeadWarHorse",
 		["ID"] = 10718,
 		["TextureVariation1"] = "MountedDeathKnightBlack_01",
 	},
-	[45] = {
+	[53] = {
 		["ModelName"] = "RidingUndeadWarHorse",
 		["ID"] = 10719,
 		["TextureVariation1"] = "MountedDeathKnightRed_01",
 	},
-	[46] = {
+	[54] = {
 		["ModelName"] = "RidingUndeadWarHorse",
 		["ID"] = 10720,
 		["TextureVariation1"] = "MountedDeathKnightGreen_01",
 	},
-	[47] = {
+	[55] = {
 		["ModelName"] = "RidingUndeadWarHorse",
 		["ID"] = 10721,
 		["TextureVariation1"] = "MountedDeathKnightPurple_01",
 	},
-	[48] = {
+	[56] = {
 		["ModelName"] = "RidingKodo",
 		["ID"] = 11641,
 		["TextureVariation1"] = "RidingKotoBeastSkinYellow",
 	},
-	[49] = {
+	[57] = {
 		["ModelName"] = "RidingKodo",
 		["ID"] = 12242,
 		["TextureVariation1"] = "RidingKotoBeastSkinTeal",
 	},
-	[50] = {
+	[58] = {
 		["ModelName"] = "RidingKodo",
 		["ID"] = 12245,
 		["TextureVariation1"] = "RidingKotoBeastSkinGreen",
 	},
-	[51] = {
+	[59] = {
 		["ModelName"] = "RidingKodo",
 		["ID"] = 12246,
 		["TextureVariation1"] = "RidingKotoBeastSkinDrab",
 	},
-	[52] = {
+	[60] = {
 		["ModelName"] = "PVPRidingFrostSabre",
 		["ID"] = 14329,
 		["TextureVariation1"] = "PVPRidingTigerSkinYellow",
 	},
-	[53] = {
+	[61] = {
 		["ModelName"] = "PVPRidingFrostSabre",
 		["ID"] = 14330,
 		["TextureVariation1"] = "PVPRidingTigerSkinBlack",
 	},
-	[54] = {
+	[62] = {
 		["ModelName"] = "PVPRidingFrostSabre",
 		["ID"] = 14331,
 		["TextureVariation1"] = "PVPRidingTigerSkinWhite",
 	},
-	[55] = {
+	[63] = {
 		["ModelName"] = "PVPRidingFrostSabre",
 		["ID"] = 14332,
 		["TextureVariation1"] = "PVPRidingTigerSkinGrey",
 	},
-	[56] = {
+	[64] = {
 		["ModelName"] = "PvPRidingDireWolf",
 		["ID"] = 14334,
 		["TextureVariation1"] = "PvPRidingDireWolfSkinDarkBlack",
 	},
-	[57] = {
+	[65] = {
 		["ModelName"] = "RidingHorsePvPT2",
 		["ID"] = 14337,
 		["TextureVariation1"] = "PvPRidingHorseSkinBlack",
 	},
-	[58] = {
+	[66] = {
 		["ModelName"] = "RidingHorsePvPT2",
 		["ID"] = 14338,
 		["TextureVariation1"] = "PvPRidingHorseSkinWhite",
 	},
-	[59] = {
+	[67] = {
 		["ModelName"] = "PvPRidingRaptor",
 		["ID"] = 14339,
 		["TextureVariation1"] = "PvPRidingRaptorSkinAqua",
 	},
-	[60] = {
+	[68] = {
 		["ModelName"] = "PvPRidingRaptor",
 		["ID"] = 14342,
 		["TextureVariation1"] = "PvPRidingRaptorSkinOrange",
 	},
-	[61] = {
+	[69] = {
 		["ModelName"] = "PvPRidingRaptor",
 		["ID"] = 14344,
 		["TextureVariation1"] = "PvPRidingRaptorSkinYellow",
 	},
-	[62] = {
+	[70] = {
 		["ModelName"] = "PVPRidingRam",
 		["ID"] = 14346,
 		["TextureVariation1"] = "PvPRidingRamSkinWhite",
 	},
-	[63] = {
+	[71] = {
 		["ModelName"] = "PVPRidingRam",
 		["ID"] = 14347,
 		["TextureVariation1"] = "PvPRidingRamSkinBrown",
 	},
-	[64] = {
+	[72] = {
 		["ModelName"] = "KodoBeastPvPT2",
 		["ID"] = 14348,
 		["TextureVariation1"] = "PVPKotoBeastSkinBlack",
 	},
-	[65] = {
+	[73] = {
 		["ModelName"] = "KodoBeastPvPT2",
 		["ID"] = 14349,
 		["TextureVariation1"] = "PVPKotoBeastSkinWhite",
 	},
-	[66] = {
+	[74] = {
 		["ModelName"] = "FrostSabre",
 		["ID"] = 14364,
 		["TextureVariation1"] = "RidingTigerSkinWhitenosaddle",
 	},
-	[67] = {
+	[75] = {
 		["ModelName"] = "PvPMechaStrider",
 		["ID"] = 14372,
 		["TextureVariation1"] = "PvPMechaStriderSkin_BlackBlack",
 	},
-	[68] = {
+	[76] = {
 		["ModelName"] = "PvPMechaStrider",
 		["ID"] = 14373,
 		["TextureVariation1"] = "PvPMechaStriderSkin_BlueBlack",
 	},
-	[69] = {
+	[77] = {
 		["ModelName"] = "PvPMechaStrider",
 		["ID"] = 14374,
 		["TextureVariation1"] = "PvPMechaStriderSkin_GreenBlack",
 	},
-	[70] = {
+	[78] = {
 		["ModelName"] = "PvPMechaStrider",
 		["ID"] = 14375,
 		["TextureVariation1"] = "PvPMechaStriderSkin_RedBlack",
 	},
-	[71] = {
+	[79] = {
 		["ModelName"] = "PvPMechaStrider",
 		["ID"] = 14376,
 		["TextureVariation1"] = "PvPMechaStriderSkin_WhiteBlack",
 	},
-	[72] = {
+	[80] = {
 		["ModelName"] = "PvPMechaStrider",
 		["ID"] = 14377,
 		["TextureVariation1"] = "PvPMechaStriderSkin_YellowBlack",
 	},
-	[73] = {
+	[81] = {
 		["ModelName"] = "PvPRidingRaptor",
 		["ID"] = 14388,
 		["TextureVariation1"] = "PvPRidingRaptorSkinDarkBlack",
 	},
-	[74] = {
+	[82] = {
+		["ModelName"] = "Gorgon101",
+		["ID"] = 14554,
+		["TextureVariation1"] = "HorseSkinEvilRed",
+	},
+	[83] = {
 		["ModelName"] = "PvPRidingDireWolf",
 		["ID"] = 14573,
 		["TextureVariation1"] = "Tier2RidingDireWolfSkinBrown",
 	},
-	[75] = {
+	[84] = {
 		["ModelName"] = "PvPRidingDireWolf",
 		["ID"] = 14574,
 		["TextureVariation1"] = "Tier2RidingDireWolfSkinGray",
 	},
-	[76] = {
+	[85] = {
 		["ModelName"] = "PvPRidingDireWolf",
 		["ID"] = 14575,
 		["TextureVariation1"] = "Tier2RidingDireWolfSkinLightGray",
 	},
-	[77] = {
+	[86] = {
 		["ModelName"] = "PVPRidingRam",
 		["ID"] = 14576,
 		["TextureVariation1"] = "PvPRidingRamSkinDarkGrey",
 	},
-	[78] = {
+	[87] = {
 		["ModelName"] = "PVPRidingRam",
 		["ID"] = 14577,
 		["TextureVariation1"] = "PvPRidingRamSkinDarkBlack",
 	},
-	[79] = {
+	[88] = {
 		["ModelName"] = "KodoBeastPvPT2",
 		["ID"] = 14578,
 		["TextureVariation1"] = "Tier2KotoBeastSkinNatural",
 	},
-	[80] = {
+	[89] = {
 		["ModelName"] = "KodoBeastPvPT2",
 		["ID"] = 14579,
 		["TextureVariation1"] = "Tier2KotoBeastSkinDrab",
 	},
-	[81] = {
+	[90] = {
 		["ModelName"] = "PvPMechaStrider",
 		["ID"] = 14581,
 		["TextureVariation1"] = "PvPMechaStriderSkin_Metal",
 	},
-	[82] = {
+	[91] = {
 		["ModelName"] = "RidingHorsePvPT2",
 		["ID"] = 14582,
 		["TextureVariation1"] = "PvPRidingHorseSkinBeige",
 	},
-	[83] = {
+	[92] = {
 		["ModelName"] = "RidingHorsePvPT2",
 		["ID"] = 14583,
 		["TextureVariation1"] = "PvPRidingHorseSkinBrown",
 	},
-	[84] = {
+	[93] = {
+		["ModelName"] = "PVPWarHorse",
+		["ID"] = 14584,
+		["TextureVariation1"] = "PVPWarHorse",
+	},
+	[94] = {
+		["ModelName"] = "Horse",
+		["ID"] = 14585,
+		["TextureVariation1"] = "HorseSkinPalamino",
+	},
+	[95] = {
+		["ModelName"] = "PVPWarHorse",
+		["ID"] = 14590,
+		["TextureVariation1"] = "PVPWarHorse",
+	},
+	[96] = {
 		["ModelName"] = "PVPRidingFrostSabre",
 		["ID"] = 14632,
 		["TextureVariation1"] = "PVPRidingTigerSkinBlackSpotted",
 	},
-	[85] = {
+	[97] = {
 		["ModelName"] = "PvPRidingDireWolf",
 		["ID"] = 14776,
 		["TextureVariation1"] = "PvPRidingDireWolfSkinFrostWolf",
 	},
-	[86] = {
+	[98] = {
 		["ModelName"] = "PVPRidingRam",
 		["ID"] = 14777,
 		["TextureVariation1"] = "PvPRidingRamSkinStormPike",
 	},
-	[87] = {
+	[99] = {
 		["ModelName"] = "PvPRidingRaptor",
 		["ID"] = 15289,
 		["TextureVariation1"] = "PvPRidingRaptorSkinGreen",
 	},
-	[88] = {
+	[100] = {
 		["ModelName"] = "PVPRidingFrostSabre",
 		["ID"] = 15290,
 		["TextureVariation1"] = "PVPRidingTigerSkinOrange",
 	},
-	[89] = {
+	[101] = {
 		["ModelName"] = "RidingSilithid",
 		["ID"] = 15677,
 		["TextureVariation1"] = "RidingSilithidTankSkinArmoredBlack",
 	},
-	[90] = {
+	[102] = {
 		["ModelName"] = "RidingSilithid",
 		["ID"] = 15678,
 		["TextureVariation1"] = "RidingSilithidTankSkinArmoredBlue",
 	},
-	[91] = {
+	[103] = {
 		["ModelName"] = "RidingSilithid",
 		["ID"] = 15679,
 		["TextureVariation1"] = "RidingSilithidTankSkinArmoredGreen",
 	},
-	[92] = {
+	[104] = {
 		["ModelName"] = "RidingSilithid",
 		["ID"] = 15680,
 		["TextureVariation1"] = "RidingSilithidTankSkinArmoredYellow",
 	},
-	[93] = {
+	[105] = {
 		["ModelName"] = "RidingSilithid",
 		["ID"] = 15681,
 		["TextureVariation1"] = "RidingSilithidTankSkinArmoredRed",
 	},
-	[94] = {
+	[106] = {
 		["ModelName"] = "RidingTurtle",
 		["ID"] = 17158,
 		["TextureVariation1"] = "TurtleMount",
